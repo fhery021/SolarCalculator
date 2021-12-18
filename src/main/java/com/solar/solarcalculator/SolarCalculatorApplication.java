8bce80f9bdaa8a156ec5c968c14b1192e1a83ffd
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-12-18 14:22:05 +0100

    making it a Spring Boot project

---
+++ src/main/java/com/solar/solarcalculator/SolarCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.solar.solarcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolarCalculatorApplication.class, args);
+    }
+
+}
